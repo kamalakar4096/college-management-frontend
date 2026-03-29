091d268416f0ab724279617deb0e1d7e0d807cf9
kamalakar4096 <kamalakarreddy@gmail.com>
2026-03-29 15:17:15 +0530

    fix: add redirects for React Router

--- src/services/authService.js
+++ src/services/authService.js
@@ -3,6 +3,6 @@ import api from '../utils/axios'
 export const authService = {
   login: async (email, password) => {
     const res = await api.post('/auth/login', { email, password })
-    return res.data
+    return res
   },
 }
